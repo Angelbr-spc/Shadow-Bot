cc3e3317253e6ef2e4d22c07d07415118d2f68b2
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-28 18:48:23 -0600

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -13,8 +13,8 @@ let handler = async (m, { conn, usedPrefix, text }) => {
       text: `📌 *Ejemplo:*\n│ .promote @usuario`,
       contextInfo: {
         externalAdReply: {
-          title: '🍷 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷',
-          body: '🍷 𝑺𝒉𝒂𝒅𝒐𝒘 𝑩𝒐𝒕 🍷',
+          title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲 🎭',
+          body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲 🎭',
           mediaType: 1,
           thumbnailUrl: 'https://files.catbox.moe/103kf9.jpg', // Puedes cambiar la imagen
           renderLargerThumbnail: false, // IMPORTANTE: esto lo deja chiquito
