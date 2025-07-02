a9af1463b4839288d32d27d78a25945ce49bb6c4
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-01 21:37:59 -0600

    Actualizar _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -5,21 +5,22 @@ let handler = async (m, { conn }) => {
   let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
 
   await conn.sendMessage(
-    m.chat,
-    {
-      image: { url: img },
-      caption: txt,
-      footer: 'Shadow Bot',
-      buttons: [
-        {
-          buttonId: '.pack',
-          buttonText: { displayText: '👉 Siguiente' },
-          type: 1
-        }
-      ],
-      headerType: 4
-    },
-    { quoted: m } // ✅ esto asegura que conteste al mensaje tocado
+  m.chat,
+  {
+    image: { url: img },
+    caption: txt,
+    footer: 'Shadow Bot',
+    buttons: [
+      {
+        buttonId: '.pack',
+        buttonText: { displayText: '👉 Siguiente' },
+        type: 1
+      }
+    ],
+    headerType: 4
+  },
+  { quoted: m.quoted ? m.quoted : m } // 👈 aquí forzas que encadene
+); // ✅ esto asegura que conteste al mensaje tocado
   );
 };
 
