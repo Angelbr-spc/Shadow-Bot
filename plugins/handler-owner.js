e8d7ad9529980e1b24b9656b8871c5b23f0b18db
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-01 22:16:15 -0600

    Actualizar handler-owner.js

--- plugins/handler-owner.js
+++ plugins/handler-owner.js
@@ -1,15 +1,1 @@
-let handler = async (m) => {
-  m.reply(`👑 *OWNER MENU* 👑
-
-Hola ${m.sender}!
-Aquí está la info del dueño del bot:
-
-- 👤 *Nombre:* Angel
-- 📱 *Número:* wa.me/5217227584934
-- 💻 *GitHub:* https://github.com/meldexzz
-- 💰 *Dona para mantener vivo este bot!*
-`)
-}
-
-handler.command = /^owner$/i
-export default handler
+//xd
