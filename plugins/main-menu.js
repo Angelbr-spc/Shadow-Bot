2d035b99417c876451cfe2e9fb393e77d9cdca6f
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-01 21:16:21 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,33 +1,38 @@
-await conn.sendMessage(
-  m.chat,
-  {
-    text: `✨ *MENÚ PRINCIPAL* ✨
+let handler = async (m, { conn }) => {
+  await conn.sendMessage(
+    m.chat,
+    {
+      text: `✨ *MENÚ PRINCIPAL* ✨
 
 Hola 👋 ${m.sender.split('@')[0]}
 Elige una opción 👇`,
-    footer: 'Bot Angel 👑',
-    templateButtons: [
-      {
-        index: 1,
-        quickReplyButton: {
-          displayText: '👑 Owner',
-          id: '.owner'
+      footer: 'Bot Angel 👑',
+      templateButtons: [
+        {
+          index: 1,
+          quickReplyButton: {
+            displayText: '📚 Info',
+            id: '.info'
+          }
+        },
+        {
+          index: 2,
+          quickReplyButton: {
+            displayText: '💸 Donar',
+            id: '.donar'
+          }
+        },
+        {
+          index: 3,
+          quickReplyButton: {
+            displayText: '👑 Owner',
+            id: '.owner'
+          }
         }
-      },
-      {
-        index: 2,
-        quickReplyButton: {
-          displayText: '💸 Donar',
-          id: '.donar'
-        }
-      },
-      {
-        index: 3,
-        quickReplyButton: {
-          displayText: '📚 Info',
-          id: '.info'
-        }
-      }
-    ]
-  }
-)
+      ]
+    }
+  )
+}
+
+handler.command = /^menu$/i
+export default handler
