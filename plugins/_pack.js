657c59b13d08560c5b573b3796785d40ce418d62
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-01 21:30:21 -0600

    Actualizar _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -4,30 +4,34 @@ let handler = async (m, { conn }) => {
   let txt = 'Pack🔥🔥🔥\n> Toca *Siguiente* para ver otro ✨';
   let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
 
-  await conn.sendMessage(m.chat, {
-    image: { url: img },
-    caption: txt,
-    footer: '🍷 Shadow Bot 🍷',
-    templateButtons: [
-      {
-        index: 1,
-        quickReplyButton: {
-          displayText: '👉 Siguiente',
-          id: '.pack'
+  await conn.sendMessage(
+    m.chat,
+    {
+      image: { url: img },
+      caption: txt,
+      footer: '🍷 Shadow Bot 🍷',
+      templateButtons: [
+        {
+          index: 1,
+          quickReplyButton: {
+            displayText: '👉 Siguiente',
+            id: '.pack'
+          }
+        }
+      ],
+      contextInfo: {
+        externalAdReply: {
+          title: '🍷 Shadow Bot 🍷',
+          body: 'Canal Oficial',
+          mediaType: 1,
+          thumbnailUrl: 'https://qu.ax/tNPfx.jpg',
+          renderLargerThumbnail: false,
+          sourceUrl: ''
         }
       }
-    ],
-    contextInfo: {
-      externalAdReply: {
-        title: '🍷 Shadow Bot 🍷',
-        body: 'Canal Oficial',
-        mediaType: 1,
-        thumbnailUrl: 'https://qu.ax/tNPfx.jpg',
-        renderLargerThumbnail: false,
-        sourceUrl: ''
-      }
-    }
-  })
+    },
+    { quoted: m } // 👈🏻 ESTO SÍ EXISTE
+  )
 }
 
 handler.command = /^pack$/i;
