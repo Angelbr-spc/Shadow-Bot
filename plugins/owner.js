7534822d61c83fc703e348d225bebb2ab76bb287
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-07 21:55:15 -0600

    Actualizar owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,58 +6,57 @@ import { createRequire } from 'module'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
-//
+
+// 🧠 Poner aquí tu número (sin el +)
+const OWNER_NUMBERS = ['5217227584934'] // <-- reemplaza con tu número
+
 let handler = async (m, _2) => {
   let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
+  const senderNumber = m.sender.split('@')[0]
+
+  // 🔒 Si no es el owner, ignorar totalmente sin mensaje
+  if (!OWNER_NUMBERS.includes(senderNumber)) return
+
   let _return
-  let _syntax = ''
-  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  let old = m.exp * 1
+  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix.trim()
+
+  if (!_text || _text.length < 3 || !/[=+\-*/{}()$]|(let|await|return|console|function)/.test(_text)) return
+
+  let err = syntaxerror(_text, 'Evaluación del dueño', {
+    allowReturnOutsideFunction: true,
+    allowAwaitOutsideFunction: true,
+    sourceType: 'module'
+  })
+
+  if (err) return
+
   try {
-    let i = 15
-    let f = {
-      exports: {}
-    }
-    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
-    _return = await exec.call(conn, (...args) => {
-      if (--i < 1) return
-      console.log(...args)
-      return conn.sendMessage(m.chat, { text: format(...args), 
-contextInfo: {
-  externalAdReply: {
-    title: '🍷 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷',
-    body: '🍷 𝑺𝒉𝒂𝒅𝒐𝒘 𝑩𝒐𝒕 🍷',
-    mediaType: 1,
-    thumbnailUrl: 'https://qu.ax/tNPfx.jpg',
-    renderLargerThumbnail: false,
-    sourceUrl: ''
-  }
-},
- }, { quoted: m })
+    let f = { exports: {} }
+    let exec = new (async () => {}).constructor(
+      'print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument',
+      _text
+    )
+
+    await exec.call(conn, (...args) => {
+      return conn.reply(m.chat, format(...args), m)
     }, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
-  } catch (e) {
-    let err = syntaxerror(_text, 'Función de ejecución', {
-      allowReturnOutsideFunction: true,
-      allowAwaitOutsideFunction: true,
-        sourceType: 'module'
-    })
-    if (err) _syntax = '```' + err + '```\n\n'
-    _return = e
-  } finally {
-    conn.reply(m.chat, _syntax + format(_return), m)
-    m.exp = old
+
+  } catch {
+    return // tampoco mostrar errores de ejecución
   }
 }
+
 handler.help = ['>', '=>']
 handler.tags = ['advanced']
 handler.customPrefix = /^=?> /
 handler.command = /(?:)/i
+handler.owner = false // para que no dispare global.dfail
 
 export default handler
 
 class CustomArray extends Array {
   constructor(...args) {
-    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
+    if (typeof args[0] === 'number') return super(Math.min(args[0], 10000))
     else return super(...args)
   }
 }
