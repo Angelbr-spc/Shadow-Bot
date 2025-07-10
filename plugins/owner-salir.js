61078c5fca8d942263ce86f8ced44827231ca11b
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-09 18:01:50 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -2,18 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.sendMessage(id, { text: `🚩 *𝐁𝐨𝐭 𝐒𝐡𝐚𝐝𝐨𝐰* Abandona El Grupo, 
-contextInfo: {
-  externalAdReply: {
-    title: '🍷 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷',
-    body: '🍷 𝑺𝒉𝒂𝒅𝒐𝒘 𝑩𝒐𝒕 🍷',
-    mediaType: 1,
-    thumbnailUrl: 'https://qu.ax/tNPfx.jpg',
-    renderLargerThumbnail: false,
-    sourceUrl: ''
-  }
-},
- }, { quoted: Fué Genial Estar Aquí 👋` }) 
+await conn.sendMessage(id, { text: `🚩 *𝐁𝐨𝐭 𝐒𝐡𝐚𝐝𝐨𝐰* Abandona El Grupo,` }) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
