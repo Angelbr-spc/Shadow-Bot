cdb471a2d8e66bfae03d8e82ca59901d9e9e50ec
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-07-12 23:53:45 -0600

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -19,11 +19,11 @@ const handler = async (m, { conn, participants, isAdmin, isOwner }) => {
   texto += `*PARA ${participants.length} MIEMBROS* 🗣️\n\n`;
   texto += `https://chat.whatsapp.com/GYOUzzKUAAq4aYgoa0pbzq?mode=r_c\n\n`; // aquí el espacio que querías
 
-  texto += `𝐆𝐫𝐮𝐩𝐨 𝐃𝐞 𝐕𝐞𝐧𝐭𝐚𝐬 𝐁𝐨𝐭𝐬:\n`;
-  texto += `𝐂𝐨𝐧𝐭𝐚𝐦𝐨𝐬 𝐂𝐨𝐧 𝐋𝐨𝐬 𝐒𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞𝐬:\n`;
+  texto += `𝐆𝐫𝐮𝐩𝐨 𝐃𝐞 𝐕𝐞𝐧𝐭𝐚𝐬 𝐁𝐨𝐭𝐬 🗣️🫵🏻:\n`;
+  texto += `𝐂𝐨𝐧𝐭𝐚𝐦𝐨𝐬 𝐂𝐨𝐧 𝐋𝐨𝐬 𝐒𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞𝐬 🗣️🫵🏻:\n`;
   texto += `\n`;
   texto += `𝐁𝐮𝐮 𝐁𝐨𝐭 🔮\n`;
-  texto += `𝐁𝐚𝐫𝐝𝐨𝐜𝐤 𝐁𝐨𝐭 \n`;
+  texto += `𝐁𝐚𝐫𝐝𝐨𝐜𝐤 𝐁𝐨𝐭 🔥\n`;
   texto += `𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷\n\n`;
 
   for (const user of participants) {
